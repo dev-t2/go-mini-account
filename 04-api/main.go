700ea41ec79912d0caab7968e0f09072d5d7f741
dev-t2 <oo_ri@naver.com>
2023-11-25 23:47:45 +0900

    04. API Update

--- 04-api/main.go
+++ 04-api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
@@ -10,10 +11,50 @@ import (
 	"github.com/gorilla/mux"
 )
 
+type User struct {
+	Id       int
+	Nickname string
+}
+
+type Users struct {
+	users []User
+}
+
+type CreateUserBody struct {
+	Nickname string
+}
+
+var users = make([]User, 0)
+
 const addr = ":8080"
 
 func main() {
 	router := mux.NewRouter()
+
+	router.HandleFunc("/users", func(res http.ResponseWriter, req *http.Request) {
+		res.Header().Set("Content-Type", "application/json")
+
+		json.NewEncoder(res).Encode(Users{users})
+	}).Methods("GET")
+
+	router.HandleFunc("/users", func(res http.ResponseWriter, req *http.Request) {
+		var body CreateUserBody
+
+		err := json.NewDecoder(req.Body).Decode(&body)
+
+		if err != nil {
+			res.WriteHeader(http.StatusBadRequest)
+			res.Write([]byte("Bad Request"))
+		}
+	}).Methods("POST")
+
+	router.HandleFunc("/users", func(res http.ResponseWriter, req *http.Request) {
+		// 
+	}).Methods("PUT")
+
+	router.HandleFunc("/users", func(res http.ResponseWriter, req *http.Request) {
+		// 
+	}).Methods("DELETE")
 
 	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
 
