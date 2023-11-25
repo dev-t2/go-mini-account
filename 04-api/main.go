e5e7143ca1a5b67408b561d0c928eb7556b9c1d7
dev-t2 <oo_ri@naver.com>
2023-11-25 10:43:35 +0900

    04. API Update

---
+++ 04-api/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+)
+
+const addr = ":8080"
+
+func main() {
+	router := mux.NewRouter()
+
+	loggedRouter := handlers.LoggingHandler(os.Stdout, router)
+
+	fmt.Printf("Server running at http://localhost%s\n", addr)
+
+	log.Fatal(http.ListenAndServe(addr, loggedRouter))
+}
