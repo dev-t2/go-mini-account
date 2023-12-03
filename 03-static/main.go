9f09c926861b937b2b008bb03f430626fa485678
dev-t2 <oo_ri@naver.com>
2023-12-03 13:47:11 +0900

    02. Gin, 03. Static and 04. API Update

--- 03-static/main.go
+++ 03-static/main.go
@@ -15,14 +15,14 @@ func main() {
 
 	router.Use(gin.Logger())
 
-	router.Use(gin.CustomRecovery(func(context *gin.Context, err any) {
-		context.String(http.StatusInternalServerError, "Internal Server Error")
+	router.Use(gin.CustomRecovery(func(ctx *gin.Context, err any) {
+		ctx.String(http.StatusInternalServerError, "Internal Server Error")
 	}))
 
 	router.Static("/", "03-static/public")
 
-	router.NoRoute(func(context *gin.Context) {
-		context.String(http.StatusNotFound, "Not Found")
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.String(http.StatusNotFound, "Not Found")
 	})
 
 	fmt.Printf("Server running at http://localhost%s\n", addr)
--- 04-api/main.go
+++ 04-api/main.go
@@ -8,6 +8,13 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	id       int
+	nickname string
+}
+
+var users = make([]User, 0)
+
 const addr = ":8080"
 
 func main() {
@@ -15,12 +22,14 @@ func main() {
 
 	router.Use(gin.Logger())
 
-	router.Use(gin.CustomRecovery(func(context *gin.Context, err any) {
-		context.String(http.StatusInternalServerError, "Internal Server Error")
+	router.Use(gin.CustomRecovery(func(ctx *gin.Context, err any) {
+		ctx.String(http.StatusInternalServerError, "Internal Server Error")
 	}))
 
-	router.NoRoute(func(context *gin.Context) {
-		context.String(http.StatusNotFound, "Not Found")
+	router.GET("/users", func(ctx *gin.Context) {})
+
+	router.NoRoute(func(ctx *gin.Context) {
+		ctx.String(http.StatusNotFound, "Not Found")
 	})
 
 	fmt.Printf("Server running at http://localhost%s\n", addr)
