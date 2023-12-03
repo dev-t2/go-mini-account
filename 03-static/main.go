6e5ebe1f2fccdb7d4a6c75dbec2fe0ce9681d6d1
dev-t2 <oo_ri@naver.com>
2023-12-03 10:46:23 +0900

    03. Static Setup

---
+++ 03-static/main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const addr = ":8080"
+
+func main() {
+	r := gin.New()
+
+	r.Use(gin.Logger())
+
+	r.Use(gin.CustomRecovery(func(c *gin.Context, err any) {
+		c.String(http.StatusInternalServerError, "Internal Server Error")
+	}))
+
+	r.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "Not Found")
+	})
+
+	err := r.Run(addr)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+}
