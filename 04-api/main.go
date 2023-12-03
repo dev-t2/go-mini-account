29854b73085be06c63993a2c7814e4a2730159ad
dev-t2 <oo_ri@naver.com>
2023-12-03 15:02:40 +0900

    04. API Create User API Update

--- 04-api/main.go
+++ 04-api/main.go
@@ -22,7 +22,31 @@ func main() {
 		ctx.String(http.StatusInternalServerError, "Internal Server Error")
 	}))
 
-	router.GET("/users", func(ctx *gin.Context) {})
+	router.GET("/users", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{"users": users})
+	})
+
+	router.POST("/users", func(ctx *gin.Context) {
+		var user User
+
+		if err := ctx.ShouldBindJSON(&user); err != nil {
+			ctx.String(http.StatusBadRequest, "Bad Request")
+
+			return
+		}
+
+		var id = 1
+		
+		if len(users) > 0 {
+			id = users[len(users) - 1].ID + 1
+		}
+
+		user.ID = id
+
+		users = append(users, user)
+
+		ctx.JSON(http.StatusCreated, user)
+	})
 
 	router.NoRoute(func(ctx *gin.Context) {
 		ctx.String(http.StatusNotFound, "Not Found")
