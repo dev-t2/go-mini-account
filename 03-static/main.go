d6894a92f27ec44194a96e8e6b6bc36fa149db9a
dev-t2 <oo_ri@naver.com>
2023-12-10 09:58:49 +0900

    03. Static Update

--- 03-static/main.go
+++ 03-static/main.go
@@ -9,19 +9,13 @@ import (
 func main() {
 	engine := gin.New()
 
-	engine.LoadHTMLGlob("03-template/views/*")
-
-	engine.Static("/public", "03-template/public")
+	engine.Static("/", "public")
 
 	engine.Use(gin.Logger())
 
 	engine.Use(gin.CustomRecovery(func(ctx *gin.Context, err any) {
 		ctx.String(http.StatusInternalServerError, "Internal Server Error")
 	}))
-
-	engine.GET("/", func(ctx *gin.Context) {
-		ctx.HTML(http.StatusOK, "home.html", gin.H{ "message" : "Hello Template" })
-	})
 	
 	engine.NoRoute(func(ctx *gin.Context) {
 		ctx.String(http.StatusNotFound, "Not Found")
